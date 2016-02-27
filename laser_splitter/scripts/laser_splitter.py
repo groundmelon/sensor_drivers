24868aff77128fbe01013a574b419eaec2b08af2
uav1 <uavteam@ust.hk>
2016-02-28 00:27:53 +0800

    update laser splitter, add outlier rejection

--- laser_splitter/scripts/laser_splitter.py
+++ laser_splitter/scripts/laser_splitter.py
@@ -9,7 +9,8 @@ import copy
 laser_range = [None, 1.9199]
 height_range = [2.059, 2.164]
 height_offset = 0.44
-
+last_h = None
+skip_cnt = 0
 
 def laser_callback(msg, pub):
     assert laser_range[1]<msg.angle_max
@@ -22,6 +23,7 @@ def laser_callback(msg, pub):
     # extract normal laser segment
     laser_msg = LaserScan()
     laser_msg.header = msg.header
+    # laser_msg.header.frame_id = "world"
     laser_msg.angle_min = msg.angle_min
     laser_msg.angle_increment = msg.angle_increment
     laser_msg.time_increment = msg.time_increment
@@ -36,7 +38,31 @@ def laser_callback(msg, pub):
                   int((height_range[1] - msg.angle_min) / msg.angle_increment))
     
     # extract height segment
-    h = numpy.mean(msg.ranges[height_idx[0]:(height_idx[1] + 1)])
+    h_lst = list(msg.ranges[height_idx[0]:(height_idx[1] + 1)])
+    h_mid = numpy.median(h_lst);
+    #print(h_mid)
+    inliers = [];
+    for x in h_lst:
+        if abs(x-h_mid)<0.10:
+            inliers.append(x)
+    # print(inliers)
+    if not inliers:
+        return
+    h = numpy.mean(inliers)
+
+    global last_h,skip_cnt
+    if last_h is None:
+        last_h = h
+    if abs(h - last_h) > 0.1:
+        skip_cnt += 1
+        if skip_cnt > 40:
+            last_h = None
+            skip_cnt = 0
+        return
+    else:
+        last_h = h
+        skip_cnt = 0
+
     height_msg = Vector3Stamped()
     height_msg.header = msg.header
     height_msg.vector.x = 0
@@ -47,14 +73,20 @@ def laser_callback(msg, pub):
     pub['laser'].publish(laser_msg)
     pub['height'].publish(height_msg)
 
+    laser_msg.ranges = h_lst
+    laser_msg.angle_min = height_range[0]
+    laser_msg.angle_max = height_range[1]
+    pub['hbeams'].publish(laser_msg)
+
 if __name__ == "__main__":
     rospy.init_node('laser_splitter')
 
     laser_pub = rospy.Publisher('~scan_out', LaserScan, queue_size=10)
     height_pub = rospy.Publisher('~height', Vector3Stamped, queue_size=10)
+    hbeams_pub = rospy.Publisher('~hbeams', LaserScan, queue_size=10)
 
     laser_sub = rospy.Subscriber('~scan_in', LaserScan, laser_callback, callback_args=dict(
-        laser=laser_pub, height=height_pub))
+        laser=laser_pub, height=height_pub, hbeams=hbeams_pub))
 
     laser_range[1]  = rospy.get_param('~laser_range_upper_limit', laser_range[1])
     height_range[0] = rospy.get_param('~height_range_lower_limit', height_range[0])
